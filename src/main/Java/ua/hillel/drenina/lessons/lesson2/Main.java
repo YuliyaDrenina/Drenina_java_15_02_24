eb9348e6b6373693e5d5a889f541add9be239e85
Yulia Drenina <jsnitko@ukr.net>
2024-02-15 23:45:07 +0300

    first commit

---
+++ src/main/Java/ua/hillel/drenina/lessons/lesson2/Main.java
@@ -0,0 +1,16 @@
+package ua.hillel.drenina.lessons.lesson2;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        byte age;
+//         byte age2 = 25;
+//        System.out.println(age2);
+
+//
+//        System.out.println(e);
+//        System.out.println(b);
+    }
+}
---
+++ src/main/Java/ua/hillel/drenina/lessons/lesson2/MyApp.java
@@ -0,0 +1,26 @@
+package ua.hillel.drenina.lessons.lesson2;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        double a=43.792461d;
+        double d=-79.047150d;
+        char g = '\u00B0';
+        char k = '\u0022';
+        byte q=43;
+        byte w=5;
+        float t=32.8596F;
+        byte u=79;
+        byte i=2;
+        double o=49.7400d;
+
+
+
+        System.out.println("Niagara Falls, NY, USA "+" Latitude = "+a + " Longitude = "+d);
+
+        System.out.println("United States country in the Cities place category" +
+                " with the gps coordinates of " + q + g + w + "'" + t + k + " N and " + u + g + i + "'" +  o + k+ " W.");
+    }
+
+
+}
