32d7ebfd6b13f646f8625d32f75f094020c8a654
Yulia Drenina <jsnitko@ukr.net>
2024-02-15 23:54:43 +0300

    new commit

--- src/main/Java/ua/hillel/drenina/lessons/lesson3/Main.java
+++ src/main/Java/ua/hillel/drenina/lessons/lesson3/Main.java
@@ -3,5 +3,6 @@ package ua.hillel.drenina.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
+        System.out.println("new commit!!!");
     }
 }
