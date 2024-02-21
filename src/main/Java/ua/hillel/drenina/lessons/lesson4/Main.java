a15670da1b12d8a043874b60988f552bb361b3bb
Yulia Drenina <jsnitko@ukr.net>
2024-02-21 21:57:28 +0300

    homework 4

---
+++ src/main/Java/ua/hillel/drenina/lessons/lesson4/Main.java
@@ -0,0 +1,18 @@
+package ua.hillel.drenina.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 20;
+        int c = 25;
+
+        long volume = a * b * c;
+        int length = a + b + c;
+
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Сумарна довжина всіх сторін =" + length);
+
+
+    }
+
+}
