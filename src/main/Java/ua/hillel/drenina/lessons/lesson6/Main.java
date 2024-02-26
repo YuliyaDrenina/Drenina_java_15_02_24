3c64d7952c17ec5d31c4b94de9610726d094d261
Yulia Drenina <jsnitko@ukr.net>
2024-02-26 23:15:38 +0300

    homework 6 review

--- src/main/Java/ua/hillel/drenina/lessons/lesson6/Main.java
+++ src/main/Java/ua/hillel/drenina/lessons/lesson6/Main.java
@@ -1,20 +1,30 @@
 package ua.hillel.drenina.lessons.lesson6;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String firstTeem = "Leleki";
-        int player1Teem1 = 5;
-        int player2Teem1 = 7;
-        int player3Teem1 = 7;
-        int player4Teem1 = 3;
-        int player5Teem1 = 8;
+        Scanner scanner=new Scanner(System.in);
 
-        String secondTeem = "Bobru";
-        int player1Teem2 = 19;
-        int player2Teem2 = 6;
-        int player3Teem2 = 2;
-        int player4Teem2 = 1;
-        int player5Teem2 = 4;
+        System.out.print("Введвть назву першої команди  ");
+        String firstTeem = scanner.next();
+        System.out.println("Ведіть фраги команди "+firstTeem);
+
+        int player1Teem1 = Integer.parseInt(scanner.next());
+        int player2Teem1 = Integer.parseInt(scanner.next());
+        int player3Teem1 = Integer.parseInt(scanner.next());
+        int player4Teem1 = Integer.parseInt(scanner.next());
+        int player5Teem1 = Integer.parseInt(scanner.next());
+
+        System.out.print("Введвть назву другої команди  ");
+
+        String secondTeem = scanner.next();
+        System.out.println("Ведіть фраги команди "+secondTeem);
+        int player1Teem2 = Integer.parseInt(scanner.next());
+        int player2Teem2 = Integer.parseInt(scanner.next());
+        int player3Teem2 = Integer.parseInt(scanner.next());
+        int player4Teem2 = Integer.parseInt(scanner.next());
+        int player5Teem2 = Integer.parseInt(scanner.next());
 
         int serednBalFirstTeem = (player1Teem1 + player2Teem1 + player3Teem1
                 + player4Teem1 + player5Teem1) / 5;
@@ -26,16 +36,15 @@ public class Main {
         int balSecondTeem = player1Teem2 + player2Teem2 + player3Teem2
                 + player4Teem2 + player5Teem2;
 
-        System.out.println("first teem ="+balFirstTeem + "   second teem ="+balSecondTeem);
-//        System.out.println(balFirstTeem > balSecondTeem);
+        System.out.println("first teem ="+serednBalFirstTeem + "   second teem ="+serednBalSecondTeem);
 
-        if(balFirstTeem==balSecondTeem){
+        if(serednBalFirstTeem==serednBalSecondTeem){
             System.out.println("Нічия!!!");
         }else {
-            if (balFirstTeem > balSecondTeem) {
-                System.out.println("Перемогла команда " + firstTeem  + " набрала " + balFirstTeem + " очків");
+            if (serednBalFirstTeem > serednBalSecondTeem) {
+                System.out.println("Перемогла команда " + firstTeem  + " набрала " + serednBalFirstTeem + " очків");
             } else {
-                System.out.println("Перемогла команда " + secondTeem  + " набрала " + balSecondTeem + " очків");
+                System.out.println("Перемогла команда " + secondTeem  + " набрала " + serednBalSecondTeem + " очків");
             }
         }
     }
